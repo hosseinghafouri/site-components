7dea32b7e251740ace70d3a4a890b52c088987f0
Hosseinghafouri <hossenghafouri@gmail.com>
2023-02-17 15:58:06 +0330

    write header part

--- components/header/header.js
+++ components/header/header.js
@@ -2,10 +2,24 @@ const template = document.createElement('template');
 template.innerHTML =`
 <link rel="stylesheet" href="/components/header/header.css">
 
-<div class="header">
-    <slot name="title"></slot>
-    <slot name="job"></slot>
-</div>
+<header class="header">
+        <h1 class="logo-name"><slot name="logo_name"></slot></h1>
+        <div class="navbar">
+            <nav>
+                <ul>
+                    <li><a href="#"><slot name="logo_name"></slot></a></li>
+                    <li><a href="#"><slot name="logo_name"></slot></a></li>
+                    <li><a href="#"><slot name="logo_name"></slot></a></li>
+                </ul>
+            </nav>
+            <img src="img/turk.png" alt="logo img">
+        </div>
+        <div class="hamburger-menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </header>
 
 `;
 class Header extends HTMLElement {
